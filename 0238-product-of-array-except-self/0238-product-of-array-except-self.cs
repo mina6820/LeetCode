63f9566f94f9088deabd216069c47383e96a958d
Mina Medhat <64274832+mina6820@users.noreply.github.com>
2024-05-05 17:59:40 +0300

    Time: 176 ms (34.73%), Space: 65.8 MB (37.16%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+
+            int[] result = new int[nums.Length];
+
+            int previous = 1;
+            int next = 1;
+
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                result[i] = previous;
+                previous *= nums[i];
+            }
+
+            for(int i = nums.Length - 1; i >= 0; i--) 
+            {
+                result[i] *= next;
+                next *= nums[i];
+            }
+        return result;
+    }
+}
