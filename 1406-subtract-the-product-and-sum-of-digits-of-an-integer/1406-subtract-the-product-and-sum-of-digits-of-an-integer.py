49b5ade320f60a92709b77d6f931cc53c54dd43e
Mina Medhat <64274832+mina6820@users.noreply.github.com>
2024-09-10 18:41:06 +0300

    Time: 32 ms (74.02%), Space: 13.9 MB (100%) - LeetHub

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/1406-subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -0,0 +1,10 @@
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+            product=1
+            sum_=0
+            while n > 0:
+                digit = n % 10
+                product *= digit
+                sum_ += digit
+                n //= 10
+            return product - sum_
